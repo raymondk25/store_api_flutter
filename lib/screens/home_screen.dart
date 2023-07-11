edd31bab2bb66d2f6e401a56d0c189fc58d18059
Raymond <raymondk25@users.noreply.github.com>
2023-07-11 13:19:41 +0800

    Implement the Search text field

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:store_api_flutter/consts/global_colors.dart';
 import 'package:store_api_flutter/widgets/appbar_icons.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +11,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -17,22 +32,53 @@ class _HomeScreenState extends State<HomeScreen> {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-        appBar: AppBar(
-          elevation: 4,
-          title: const Text("Home"),
-          leading: AppBarIcons(
-            function: () {},
-            icon: IconlyBold.category,
-          ),
-          actions: [
-            AppBarIcons(
+          appBar: AppBar(
+            elevation: 4,
+            title: const Text("Home"),
+            leading: AppBarIcons(
               function: () {},
-              icon: IconlyBold.user3,
-            )
-          ],
-        ),
-        body: Center(child: Text("Home Screen")),
-      ),
+              icon: IconlyBold.category,
+            ),
+            actions: [
+              AppBarIcons(
+                function: () {},
+                icon: IconlyBold.user3,
+              )
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(height: 18),
+                TextField(
+                  controller: _textEditingController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      hintText: "Search",
+                      filled: true,
+                      fillColor: Theme.of(context).cardColor,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          color: Theme.of(context).cardColor,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                      suffixIcon: Icon(
+                        IconlyLight.search,
+                        color: lightIconsColor,
+                      )),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
