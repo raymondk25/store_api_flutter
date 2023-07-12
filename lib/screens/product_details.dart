2529705cf5114516511808c46462a0a36825efda
Raymond <raymondk25@users.noreply.github.com>
2023-07-12 19:52:19 +0800

    Error Handling

--- lib/screens/product_details.dart
+++ lib/screens/product_details.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:card_swiper/card_swiper.dart';
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
@@ -21,9 +23,17 @@ class ProductDetails extends StatefulWidget {
 class _ProductDetailsState extends State<ProductDetails> {
   final titleStyle = const TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
   ProductsModel? productsModel;
+  bool isError = false;
+  String errStr = "";
 
   Future<void> getProductInfo() async {
-    productsModel = await APIHandler.getProductById(id: widget.id);
+    try {
+      productsModel = await APIHandler.getProductById(id: widget.id);
+    } catch (e) {
+      isError = true;
+      errStr = e.toString();
+      log("error $e");
+    }
     setState(() {});
   }
 
@@ -38,106 +48,113 @@ class _ProductDetailsState extends State<ProductDetails> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: productsModel == null
-            ? const Center(child: CircularProgressIndicator())
-            : SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    const SizedBox(
-                      height: 18,
-                    ),
-                    const BackButton(),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            productsModel!.category.toString(),
-                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                          ),
-                          const SizedBox(
-                            height: 18,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: isError
+            ? Center(
+                child: Text(
+                  "An error occured $errStr",
+                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+              )
+            : productsModel == null
+                ? const Center(child: CircularProgressIndicator())
+                : SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        const BackButton(),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Flexible(
-                                flex: 3,
-                                child: Text(
-                                  productsModel!.title.toString(),
-                                  textAlign: TextAlign.start,
-                                  style: titleStyle,
-                                ),
+                              Text(
+                                productsModel!.category!.name.toString(),
+                                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
                               ),
-                              Flexible(
-                                flex: 1,
-                                child: RichText(
-                                  text: TextSpan(
-                                      text: '\$',
-                                      style: const TextStyle(fontSize: 25, color: Color.fromRGBO(33, 150, 243, 1)),
-                                      children: <TextSpan>[
-                                        TextSpan(
-                                            text: productsModel!.price.toString(),
-                                            style: TextStyle(color: lightTextColor, fontWeight: FontWeight.bold)),
-                                      ]),
-                                ),
+                              const SizedBox(
+                                height: 18,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    flex: 3,
+                                    child: Text(
+                                      productsModel!.title.toString(),
+                                      textAlign: TextAlign.start,
+                                      style: titleStyle,
+                                    ),
+                                  ),
+                                  Flexible(
+                                    flex: 1,
+                                    child: RichText(
+                                      text: TextSpan(
+                                          text: '\$',
+                                          style: const TextStyle(fontSize: 25, color: Color.fromRGBO(33, 150, 243, 1)),
+                                          children: <TextSpan>[
+                                            TextSpan(
+                                                text: productsModel!.price.toString(),
+                                                style: TextStyle(color: lightTextColor, fontWeight: FontWeight.bold)),
+                                          ]),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 18,
                               ),
                             ],
                           ),
-                          const SizedBox(
-                            height: 18,
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: size.height * 0.4,
-                      child: Swiper(
-                        itemBuilder: (BuildContext context, int index) {
-                          return FancyShimmerImage(
-                            width: double.infinity,
-                            imageUrl: productsModel!.images![index].toString(),
-                            boxFit: BoxFit.fill,
-                          );
-                        },
-                        autoplay: true,
-                        itemCount: 3,
-                        pagination: const SwiperPagination(
-                          alignment: Alignment.bottomCenter,
-                          builder: DotSwiperPaginationBuilder(
-                            color: Colors.white,
-                            activeColor: Colors.red,
+                        ),
+                        SizedBox(
+                          height: size.height * 0.4,
+                          child: Swiper(
+                            itemBuilder: (BuildContext context, int index) {
+                              return FancyShimmerImage(
+                                width: double.infinity,
+                                imageUrl: productsModel!.images![index].toString(),
+                                boxFit: BoxFit.fill,
+                              );
+                            },
+                            autoplay: true,
+                            itemCount: 3,
+                            pagination: const SwiperPagination(
+                              alignment: Alignment.bottomCenter,
+                              builder: DotSwiperPaginationBuilder(
+                                color: Colors.white,
+                                activeColor: Colors.red,
+                              ),
+                            ),
+                            // control: const SwiperControl(),
                           ),
                         ),
-                        // control: const SwiperControl(),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 18,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Description', style: titleStyle),
-                          const SizedBox(
-                            height: 18,
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Description', style: titleStyle),
+                              const SizedBox(
+                                height: 18,
+                              ),
+                              Text(
+                                productsModel!.description.toString(),
+                                textAlign: TextAlign.start,
+                                style: const TextStyle(fontSize: 25),
+                              ),
+                            ],
                           ),
-                          Text(
-                            productsModel!.description.toString(),
-                            textAlign: TextAlign.start,
-                            style: const TextStyle(fontSize: 25),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
+                  ),
       ),
     );
   }
--- lib/services/api_handler.dart
+++ lib/services/api_handler.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:http/http.dart' as http;
 import 'package:store_api_flutter/consts/api_consts.dart';
@@ -8,22 +9,36 @@ import 'package:store_api_flutter/models/users_model.dart';
 
 class APIHandler {
   static Future<List<dynamic>> getData({required String target}) async {
-    Uri uri = Uri.https(kBaseUrl, "api/v1/$target");
-    var response = await http.get(uri);
-    // print("response: ${jsonDecode(response.body)}");
-    var data = jsonDecode(response.body);
-    List tempList = [];
-    for (var v in data) {
-      tempList.add(v);
+    try {
+      Uri uri = Uri.https(kBaseUrl, "api/v1/$target");
+      var response = await http.get(uri);
+
+      var data = jsonDecode(response.body);
+      if (response.statusCode != 200) throw data["message"];
+
+      List tempList = [];
+      for (var v in data) {
+        tempList.add(v);
+      }
+      return tempList;
+    } catch (e) {
+      log("An error occured $e");
+      throw e.toString();
     }
-    return tempList;
   }
 
   static Future<ProductsModel> getProductById({required String id}) async {
-    Uri uri = Uri.https(kBaseUrl, "api/v1/products/$id");
-    var response = await http.get(uri);
-    var data = jsonDecode(response.body);
-    return ProductsModel.fromJson(data);
+    try {
+      Uri uri = Uri.https(kBaseUrl, "api/v1/products/$id");
+      var response = await http.get(uri);
+      var data = jsonDecode(response.body);
+      if (response.statusCode != 200) throw data["message"];
+
+      return ProductsModel.fromJson(data);
+    } catch (e) {
+      log("An error occured while getting product info $e");
+      throw e.toString();
+    }
   }
 
   static Future<List<ProductsModel>> getAllProducts() async {
